68d9902425ba56a0f5f671063820b8fa0f0466e3
hconps <82867440+hconps@users.noreply.github.com>
2026-09-19 00:13:32 +0800

    Update xray-pre-release.sh

--- docker/xray-pre-release.sh
+++ docker/xray-pre-release.sh
@@ -207,7 +207,7 @@ docker compose up -d
 
 # --- 9. 输出链接 ---
 # 链接中的指纹 fingerprint 建议用 chrome 或 random，这里用 chrome
-FINGERPRINT="chrome"
+FINGERPRINT="android"
 VLESS_URL="vless://$UUID@${IP}:${PORT}?encryption=none&security=reality&type=tcp&sni=$DOMAIN&fp=$FINGERPRINT&pbk=$PUBLIC_KEY&sid=$SHORT_ID&flow=xtls-rprx-vision#Xray_Reality_${IP}"
 
 echo "$VLESS_URL" > ~/xray/url
